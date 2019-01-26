7bc4ec445828fbf1fc2e155b46c9e59ad6e4af7f
Danny Yassine <dannyyassine@gmail.com>
2019-01-25 22:51:50 -0500

    push

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$http = new Swoole\Http\Server("3.92.223.16", 9501);
+$http = new Swoole\Http\Server("127.0.0.1", 9501);
 
 $http->on("start", function ($server) {
-    echo "Swoole http server is started at http://3.92.223.16:9501\n";
+    echo "Swoole http server is started at http://127.0.0.1:9501\n";
 });
 
 $http->on("request", function ($request, $response) {
