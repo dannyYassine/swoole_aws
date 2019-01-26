b8a125de299fec5c28a2ead8d573ac7de96d0a1c
Danny Yassine <dannyyassine@gmail.com>
2019-01-26 10:16:43 -0500

    clean up

--- app/index.php
+++ app/index.php
@@ -3,5 +3,17 @@ define('APP_ROOT', __DIR__);
 
 require 'utils/View.php';
 
-$app = new \Slim\App;
+$c = new \Slim\Container(); //Create Your container
+
+//Override the default Not Found Handler before creating App
+$c['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $response->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write(View::get('404'));
+
+    };
+};
+
+$app = new \Slim\App($c);
 require 'routes.php';
