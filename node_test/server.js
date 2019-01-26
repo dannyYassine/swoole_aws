46717d0492c6d09887171b0ede3dedf263ef243d
Danny Yassine <dannyyassine@gmail.com>
2019-01-26 12:56:47 -0500

    clean up

---
+++ node_test/server.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+const port = 9501;
+const path = require('path');
+
+app.use(express.static('/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/app/views/index.html'), function (html) {
+        res.send(html);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
