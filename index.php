3e7f315b5aef24134011ff559789350449d76b6b
Danny Yassine <dannyyassine@gmail.com>
2019-01-25 22:39:29 -0500

    initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$http = new Swoole\Http\Server("127.0.0.1", 9501);
+
+$http->on("start", function ($server) {
+    echo "Swoole http server is started at http://127.0.0.1:9501\n";
+});
+
+$http->on("request", function ($request, $response) {
+    $response->header("Content-Type", "application/json");
+    $response->send("{id:1}");
+});
+
+$http->start();
