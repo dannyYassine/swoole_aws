c2207dd341719056e7a453985115783d15308300
Danny Yassine <dannyyassine@gmail.com>
2019-01-26 09:42:36 -0500

    clean up

--- public/js/map.js
+++ public/js/map.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($) {
 
 	//google map custom marker icon - .png fallback for IE11
 	var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-	var $marker_url = ( is_internetExplorer11 ) ? 'assets/img/location.png' : 'assets/img/location.png';
+	var $marker_url = ( is_internetExplorer11 ) ? 'public/img/location.png' : 'public/img/location.png';
 		
 	//define the basic color of your map, plus a value for saturation and brightness
 	var	$main_color = '#2d313f',
